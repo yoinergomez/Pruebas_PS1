003b453a8058fa02ed74b2a0783c36c5133f5483
frankalexis <castrillonfrank114@gmail.com>
2017-08-20 11:38:59 -0500

    Se añaden los comentarios a la clase Analizar y Estadistica

--- src/main/java/co/edu/udea/pruebas_ps1/comprobar/Analizar.java
+++ src/main/java/co/edu/udea/pruebas_ps1/comprobar/Analizar.java
@@ -7,7 +7,11 @@ package co.edu.udea.pruebas_ps1.comprobar;
  * @version v1
  */
 public class Analizar {
-    
+    /**
+     * Verifica si una sentencia es un clase
+     * @param linea a comprobar
+     * @return true si es una clase o false en otro caso
+     */
     public boolean comprobarClase(String linea){
         String aux[] = linea.split(" ");
         if (aux[0].equals("class")){
@@ -17,7 +21,11 @@ public class Analizar {
         }
         return false;
     }
-    
+    /**
+     * Verifica si una linea es un método
+     * @param linea a verificar
+     * @return true si es un meétodo o false en caso contrario
+     */
     public boolean comprobarMetodo(String linea){
         String aux;
         if (linea.contains("throws")){
